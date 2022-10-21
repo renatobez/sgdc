94c02ffa1eac080b2048fd23b3e2d2e981369ebc
rbsdev <renatos725@gmail.com>
2022-10-20 23:56:02 -0300

    testando commit local

--- sections/painel.php
+++ sections/painel.php
@@ -9,119 +9,59 @@
     <title>rbsCoud</title>
 </head>
 <body class="painel">
-
-<!-- criando o navbar -->
-<nav class="navbar navbar-expand-lg navbar-light navbar-painel">
-
-    <h4 class="text-logo">rbs.<span class="text-logo-span">Cloud</span></h4>
-
+<!-- navegação topo -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#"><img src="../_img/logo-login2.png" width="100px" alt=""></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
-        <!-- input de pesquisa interno AJAX-->
-        <form class="form-inline mx-auto">
-            <input class="form-control mr-sm-2" type="search" placeholder="buscar no sistema " aria-label="pesquisar" size="70%" style="margin-left: 30%">
-        </form>
-
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="nav-link " href="#" style="font-size: 1.4rem"><i class="bi bi-person-circle icons-navbar"></i></a>
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(página atual)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link " href="#" style="font-size: 1.4rem"><i class="bi bi-gear"></i></a>
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Ação</a>
+                    <a class="dropdown-item" href="#">Outra ação</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Algo mais aqui</a>
+                </div>
             </li>
             <li class="nav-item">
-                <a href="../index.php" class="nav-link " style="font-size: 1.4rem"><i class="bi bi-box-arrow-left icons-navbar"></i></a>
+                <a class="nav-link disabled" href="#">Desativado</a>
             </li>
         </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+        </form>
     </div>
 </nav>
-
-
-
-<!-- criando o painel principal -->
-
-<div class="d-flex">
-    <nav class="sidebar">
-        <a href="painel.php" style="font-weight: bold"><p class="titles-dashboard"><i class="bi bi-speedometer2 icons-navbar"></i> &nbsp;Dashboard Geral</p></a>
-        <a href="desenvolvimento.php"><p class="titles-dashboard"><i class="bi bi-code-slash icons-navbar"></i> &nbsp;Desenvolvimento Web</p></a>
-        <a href="mdigital.php"><p class="titles-dashboard"><i class="bi bi-bullseye"></i> &nbsp;Marketing Digital</p></a>
-        <a href="tutoriais.php"><p class="titles-dashboard"><i class="bi bi-terminal-dash icons-navbar"></i> &nbsp;Tutoriais & Dicas</p></a>
-        <a href="downloads.php"><p class="titles-dashboard"><i class="bi bi-cloud-download icons-navbar"></i> &nbsp;Downloads</p></a>
-        <a href="uploads.php"><p class="titles-dashboard"><i class="bi bi-cloud-upload"></i> &nbsp;Upload de material</p></a>
-        <a href="settings.php"><p class="titles-dashboard"><i class="bi bi-gear icons-navbar"></i> &nbsp;Configurações</p></a>
-    </nav>
-
-    <!-- criando a tela de conteúdo-->
-    <div class="content p-2">
-        <div class="list-group-item box-content">
-            <div class="mr-auto p-2">
-                <h3 class="display-4 title-content">Painel Dashboard</h3>
+<!--painel principal -->
+    <div class="row m-1">
+        <div class="col-3">
+            <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
+                <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Cadastro</a>
+                <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Consulta</a>
+                <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Configurações</a>
             </div>
-           <div class="d-flex justify-content-around">
-               <!-- sessão cards -->
-               <div class="row mb-2">
-                    <div class="card bg-light mx-3 ">
-                        <div class="card-body">
-                            <div class="card-title">
-                                <h5>Conteúdo cadastrados</h5>
-                                <div class="card-text">
-
-                                </div>
-                            </div>
-                        </div>
-                            <div class="card-footer">
-                                <small class="text-muted">Atualizados 3 minutos atrás</small>
-                            </div>
-                    </div>
-
-                   <div class="card bg-light mx-3">
-                       <div class="card-body">
-                           <div class="card-title">
-                               <h5>Conteúdo cadastrados</h5>
-                               <div class="card-text">
-
-                               </div>
-                           </div>
-                       </div>
-                       <div class="card-footer">
-                           <small class="text-muted">Atualizados 3 minutos atrás</small>
-                       </div>
-                   </div>
-
-                   <div class="card bg-light mx-3">
-                       <div class="card-body">
-                           <div class="card-title">
-                               <h5>Conteúdo cadastrados</h5>
-                               <div class="card-text">
-
-                               </div>
-                           </div>
-                       </div>
-                       <div class="card-footer">
-                           <small class="text-muted">Atualizados 3 minutos atrás</small>
-                       </div>
-                   </div>
-
-                   <div class="card bg-light mx-3">
-                       <div class="card-body">
-                           <div class="card-title">
-                               <h5>Conteúdo cadastrados</h5>
-                               <div class="card-text">
-
-                               </div>
-                           </div>
-                       </div>
-                       <div class="card-footer">
-                           <small class="text-muted">Atualizados 3 minutos atrás</small>
-                       </div>
-                   </div>
-
-               </div>
-
-           </div>
+        </div>
+        <div class="col-9">
+            <div class="tab-content bg-light" id="v-pills-tabContent">
+                <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">Home</div>
+                <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">Cadastro</div>
+                <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">Consulta</div>
+                <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">Configuração</div>
+            </div>
         </div>
     </div>
 </div>
