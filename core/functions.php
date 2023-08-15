72895250e9cfedf20a4f88999b4ce057a312646d
rbsdev <renatobezerrasilva@hotmail.com>
2023-08-14 23:54:53 -0300

    Alterações na estrutura de pastas.

--- core/functions.php
+++ core/functions.php
@@ -8,16 +8,6 @@
 
 function mostrarHora (){
 
-    $date = date("d/m/Y H:i");
+    echo date("d/m/Y H:i");
 
-    if($date >= 4 && $date <=11){
-
-        echo "Bom dia! {$date}";
-    }elseif ($date >= 12 && $date <=18){
-
-        echo "Boa tarde! {$date}";
-    }else{
-
-        echo "Boa noite! {$date}";
-    }
 }
